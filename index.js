3eaf7e48e804e5b89b94c34ce11fd327b837a1ee
Danny Wakeling <wakeling101@hotmail.com>
2019-02-12 08:52:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,20 +5,19 @@ function takeANumber(currentLine, name){
 
 function nowServing(currentLine){
   if(currentLine.length) {
-    return `Currently serving ${currentLine.shift()}.`
+    return `Currently serving ${currentLine.shift()}.` // returns value and removes it
   } else {return "There is nobody waiting to be served!"}
 }
 
-
 function currentLine(x) {
-    var line = []
+    let line = []
     if (x.length === 0) {
       return "The line is currently empty."
     } else {
-      for(var i = 0; i < x.length; i++) {
+      for(let i = 0; i < x.length; i++){
         line += (i + 1) + ". " + x[i] + ", "
       }
-      line = line.slice(0, line.length-2)
+      line = line.slice(0, line.length-2) // removes the last comma and space
       return "The line is currently: " + line
     }
 }
